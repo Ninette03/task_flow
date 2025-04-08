6491a049e3b187cd5fc354b9298b6bfc8db0ff12
Ninette03 <n.agatesi@alustudent.com>
2025-04-08 16:08:05 +0200

    Fixed double declaration in taskRoutes

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTask, getAllTasks, updateTask, deleteTask, getAllTasks, getUserTasks, markTaskCompleted } from "../controllers/taskController.js";
+import { createTask, getAllTasks, updateTask, deleteTask, getUserTasks, markTaskCompleted } from "../controllers/taskController.js";
 import { verifyToken, isAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
